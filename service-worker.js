fd13da16cc6cbecc92ed7a0dd69bc2847be8951a
LauraOzolina <laura.ozolina.latvia@gmail.com>
2025-09-04 17:09:24 +0300

    service worker fix

--- service-worker.js
+++ service-worker.js
@@ -5,8 +5,3 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
   // Can add caching later
 });
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("service-worker.js")
-    .then(reg => console.log("Service Worker registered:", reg))
-    .catch(err => console.log("SW registration failed:", err));
-}
